913566f6716e7d27079be4dfc35f597e1e980604
Nokolai Bel <kolabelorus@gmail.com>
2022-02-24 22:30:26 +0300

    fix:decode

--- src/index.js
+++ src/index.js
@@ -39,6 +39,22 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let num = [];
+    let value;
+    num = expr.match(/.{0,10}/g)
+    .map((value) => {
+        value = value.substring(value.indexOf('1'))
+        .replace(/10/g,'.')
+        .replace(/11/g,'-')
+        .replace(/\*\*\*\*\*\*\*\*\*\*/g, " ");
+        Object.keys(MORSE_TABLE).map((e) => {
+            if(e === value) {
+                value = MORSE_TABLE[e];
+            }
+        });
+        return value;
+    });
+    return num.join('');
 }
 
 module.exports = {
